2425b47193fc8d16dc2cc374d1e998fbe2f05ea0
Qiang Zhao <koqizhao@outlook.com>
2018-09-21 18:51:31 +0800

    add spring boot

---
+++ study/spring-boot/src/main/java/io/study/springboot/App.java
@@ -0,0 +1,18 @@
+package io.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+import io.study.springboot.example.Example;
+
+/**
+ * @author koqizhao
+ *
+ * Sep 21, 2018
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+
+}

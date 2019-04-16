231821dc4f7d45ff70307fec84103c2da4c4099a
Qiang Zhao <koqizhao@outlook.com>
2019-04-16 16:04:06 +0800

    add spring-cloud-study project

---
+++ spring-cloud-study/spring-cloud-commons/src/main/java/io/study/springcloud/App.java
@@ -0,0 +1,18 @@
+package io.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+
+import io.study.springcloud.example.Example;
+
+/**
+ * @author koqizhao
+ *
+ * Sep 21, 2018
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
---
+++ spring-cloud-study/spring-cloud-commons/src/main/java/io/study/springcloud/example/Example.java
@@ -0,0 +1,21 @@
+package io.study.springcloud.example;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author koqizhao
+ *
+ * Sep 21, 2018
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+}
